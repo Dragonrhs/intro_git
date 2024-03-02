7d970f37e0a03ea135bee866b1c149bedb427288
dragonrhs <ryuske2009@hotmail.com>
2024-03-02 12:06:49 -0300

    refactor(intro-git): usa calculadora para fazer soma

--- teste_calculadora.py
+++ teste_calculadora.py
@@ -2,6 +2,7 @@ import calculadora
 def main():
     a = 2
     b = 3
-    calculadora.soma(a, b)
-    print('f{a}+{b}={a+b}')
+    soma = calculadora.soma(a, b)
+    print(f'{a}+{b}={soma}')
+    
     main()
